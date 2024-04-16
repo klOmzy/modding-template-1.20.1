a7b5f05db56dc98dfb823895c238b679ce82d84b
klOmzy <klOmzyy@gmail.com>
2024-04-16 21:56:19 +0600

    first commit

---
+++ src/main/java/erloo/modding/ModdingClient.java
@@ -0,0 +1,11 @@
+package erloo.modding;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModdingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
